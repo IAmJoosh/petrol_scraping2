2dee31e7dc9485e30c9fc104285b8f2d5088c92c
Joshua Whittaker <whittaker.d.joshua@gmail.com>
2023-04-24 17:39:56 +0200

    Added urls

---
+++ urls.py
@@ -0,0 +1,6 @@
+# HTML
+CALTEX = 'https://caltex.co.za/motorists/products-and-services/fuel-price.html'
+SHELL  = 'https://www.shell.co.za/motorists/shell-fuels/petrol-price.html'
+
+# API
+ENGEN  = 'https://engen-admin.engen.co.za/api/country/fuel-prices'
